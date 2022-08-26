321862340001763fb0d1c5d7e5e273e4a6f19b5d
DumasOlivier <dumas.olivier@outlook.fr>
2022-08-26 17:22:33 +0200

    Task Entity

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { TaskStatus } from './task.model';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
